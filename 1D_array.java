b46c565b2d42c9f41c0c0e1e37fb9ccdb561b326
Ankita Daghottra <69582277+ankitadaghottra@users.noreply.github.com>
2021-07-10 11:32:27 +0530

    Add files via upload

---
+++ 1D_array.java
@@ -0,0 +1,18 @@
+class 1D_array
+{
+
+ public static void main(String[] args)
+  {
+      int[] numbers = new int[5];
+      number[0] = 20;
+      number[1] = 30;
+      number[2] = 40;
+      number[3] = 50;
+      number[4] = 60;
+      for(int i=0; i<number.length; i++)
+          System.out.println(number[i]);
+  }
+  
+
+
+}
